b5eddf1048d61abd0f6f761da22f670423303484
Moe Quinn <moequinn50@gmail.com>
2019-01-30 18:34:20 -0500

    add

--- interactive_story/sketch.js
+++ interactive_story/sketch.js
@@ -8,58 +8,60 @@
 function setup() {
     
     createCanvas(800, 500);
+    textSize(40);
+    textAlign();
 
 }
 
 function draw() {
     
-    background(204, 255, 229)
+    background(204, 255, 229) // background color
     
     //Characater 1 on the left Joe
     //Characater 2 on the right Abel
     
     
-    //Character 1 eye colors
+    //Character 1 
     noStroke();
-    fill(0);
+    fill(0);  // eye colors
     
-    // Characater 1 left eye
-    ellipse(185, 275, 20, 20);
+    // Characater 1 
+    ellipse(185, 275, 20, 20);  // left eye
     
-    // Characater 1 right eye
-    ellipse(215, 275, 20, 20);
+    // Characater 1 
+    ellipse(215, 275, 20, 20);  // right eye
     
-    //Character 1 head and body color
+    //Character 1 
     noStroke();
-    fill(102, 178, 255);
+    fill(102, 178, 255);  // head and body color
     
-    // Characater 1 Head
-    ellipse(200, 275, 15, 30);
+    // Characater 1 
+    ellipse(200, 275, 15, 30);  // head
     
-    // Characater 1 Body
-    ellipse(200, 375, 20, 200);
+    // Characater 1 
+    ellipse(200, 375, 20, 200);  // Body
     
     
     
-    //Character 2 eye colors
+    //Character 2 
     noStroke();
-    fill(30);
+    fill(30);  // eye colors
     
-    // Character 2 left eye
-    ellipse(570, 300, 30, 30);
+    // Character 2 
+    ellipse(570, 300, 30, 30);  // left eye
     
-    // Characater 2 right eye
-    ellipse(630, 300, 30, 30);
+    // Characater 2 
+    ellipse(630, 300, 30, 30);  // right eye
     
     //Character 2
     noStroke();
     fill(255, 255, 51);
     
-    // Character 2 Head
-    ellipse(600, 300, 60, 50);
+    // Character 2 
+    ellipse(600, 300, 60, 50);  // head
     
-    // Characater 2 top Body 
-    ellipse(600, 375, 100, 150);
+    // Characater 2  
+    ellipse(600, 375, 100, 150);  // top Body
     
     
     
@@ -67,34 +69,34 @@ function draw() {
     noStroke();
     fill(0);
     
-    // Character 2 Head
-    ellipse(600, 300, 50, 40);
+    // Character 2 
+    ellipse(600, 300, 50, 40); // head
     
-    // Characater 2 top Body 
-    ellipse(600, 375, 90, 140);
+    // Characater 2  
+    ellipse(600, 375, 90, 140); // top Body
     
     
-    //Character 2 layer 2
+    //Character 2 
     noStroke();
-    fill(255, 255, 51);
+    fill(255, 255, 51); // layer 2
     
-    // Character 2 Head
-    ellipse(600, 300, 40, 30);
+    // Character 2 
+    ellipse(600, 300, 40, 30); // head
     
-    // Characater 2 top Body 
-    ellipse(600, 375, 80, 130);
-    
-    
-    //Character 2 layer 3
-    noStroke();
-    fill(0);
-    
-    // Character 2 Head
-    ellipse(600, 300, 50, 40);
-    
-    // Characater 2 top Body 
-    ellipse(600, 375, 70, 120);
+    // Characater 2  
+    ellipse(600, 375, 80, 130); // top Body
     
     
+    //Character 2 
+    noStroke(); 
+    fill(0);  // layer 3
+    
+    // Character 2 
+    ellipse(600, 300, 50, 40); // head
+    
+    // Characater 2 
+    ellipse(600, 375, 70, 120);  // top Body 
+    
+    text('Meet Joe and Abel', 240, 120); //comments
   
 }
